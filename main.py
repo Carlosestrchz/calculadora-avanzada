bb0f4c6ffe5ecd93a1e4a36bb1edfe077979222a
Carlos Ignacio Estrada Chairez <carlosestrchz@gmail.com>
2026-09-16 14:45:09 -0600

    feature: se implementa logica requerida para el reto

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+import math
+
+def addmultiplenumbers(numbers):
+    return sum(numbers)
+
+def multiplymultiplenumbers(numbers):
+    return math.prod(numbers)
+
+def isiteven(num):
+    return isinstance(num, int) and num % 2 == 0
+
+def isitaninteger(num):
+    return isinstance(num, int) or (isinstance(num, float) and num.is_integer())
+
 def main():
   print("Hello learners!")
 
